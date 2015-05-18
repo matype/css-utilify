152bee6f717324d1d7f884883b8503267129e1c2
Masaaki Morishita <ikasama3.0@gmail.com>
2015-05-18 13:51:47 +0900

    implement cli

--- cli.js
+++ cli.js
@@ -1,4 +1,45 @@
 #!/usr/bin/env node
 
+var fs = require('fs')
 var pkg = require('./package.json')
-var cssUtilify = require('./')
+var postcss = require('postcss')
+var utilify = require('./').utilify
+
+var minimist = require('minimist')
+var argv = minimist(process.argv.slice(2), {
+    boolean: [
+        'help',
+        'versions'
+    ],
+    alias: {
+        c: 'compress',
+        h: 'help',
+        V: 'versions'
+    }
+})
+
+if (argv.V) console.log(pkg.version)
+
+if (argv.h) {
+    console.log('Usage: css-utilify input-name output-name [options]');
+    console.log('');
+    console.log('Options:');
+    console.log('');
+    console.log('  -V, --versions    output the version number');
+    console.log('  -h, --help        output usage information');
+}
+
+if (argv._[0]) {
+    var input = argv._[0]
+    var fileName = input.split('.')[0]
+    var extension = input.split('.')[1]
+    if (extension !== 'css') throw new Error('Input file must be a CSS file')
+
+    var css = fs.readFileSync(input, 'utf-8')
+    var root = postcss.parse(css)
+    var output = argv._[1] || fileName + '.base.css'
+    fs.writeFile(output, utilify(root), function (err) {
+        if (err) throw err
+        console.log('Successed')
+    })
+}
--- index.js
+++ index.js
@@ -4,10 +4,18 @@ module.exports = postcss.plugin('css-utilify', function (options) {
     options = options || {}
 
     return function (root) {
-        var baseAnnotation = postcss.comment({text: '@base'}).toString()
-
-        root.eachRule(function (rule) {
-            rule.prepend(baseAnnotation)
-        })
+        return utilify(root)
     }
 })
+
+function utilify (root) {
+    var baseAnnotation = postcss.comment({ text: '@base' }).toString()
+
+    root.eachRule(function (rule) {
+        rule.prepend(baseAnnotation)
+    })
+
+    return root
+}
+
+module.exports.utilify = utilify
