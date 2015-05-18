342bcee46fe01f19b84ffc74e3b0b5ec0e035e37
Masaaki Morishita <ikasama3.0@gmail.com>
2015-05-18 12:30:31 +0900

    implement as a postcss plugin

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = 
+var postcss = require('postcss')
+
+module.exports = postcss.plugin('css-utilify', function (options) {
+    options = options || {}
+
+    return function (root) {
+        var baseAnnotation = postcss.comment({text: '@base'}).toString()
+
+        root.eachRule(function (rule) {
+            rule.prepend(baseAnnotation)
+        })
+    }
+})
--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-var cssUtilify = require('./')
+var fs = require('fs')
+var utilify = require('./')
+var test = require('tape')
+
+var css = fs.readFileSync('fixture.css', 'utf-8')
+var expected = fs.readFileSync('expected.css', 'utf-8')
+
+test('in postcss', function (t) {
+    var postcss = require('postcss')
+    var actual = postcss().use(utilify()).process(css).css
+    t.same(actual, expected)
+    t.end()
+})
